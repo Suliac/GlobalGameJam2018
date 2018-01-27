63518f9cc1c99113fd88471513d155430e317691
Stonga <gaston.robert.auditeur@lecnam.net>
2018-01-27 15:24:28 +0100

    commit pour premiers tests de son

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioClip[] Clips;
+    private AudioSource[] audioSources;
+
+    // Use this for initialization
+    void Start () {
+
+        audioSources = new AudioSource[Clips.Length];
+        int i = 0;
+        while (i < Clips.Length)
+        {
+            GameObject child = new GameObject("Player");
+
+            child.transform.parent = gameObject.transform;
+
+            audioSources[i] = child.AddComponent<AudioSource>() as AudioSource;
+
+            audioSources[i].clip = Clips[i];
+
+            i++;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
