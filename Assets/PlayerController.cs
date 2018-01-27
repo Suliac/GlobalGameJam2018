b264955e93f5a653ed85da488d06de65fdce3394
Suliac Blineau <suliac.blineau@orange.fr>
2018-01-27 10:20:25 +0100

    Start simple code

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject policeSpawn;
+    public GameObject profilerSpawn;
+    // Use this for initialization
+    void Start()
+    {
+        if (isServer)
+        {
+            // Policier
+            print("Je suis un policier");
+        }
+        else
+        {
+            // Profiler
+            print("Je suis un profiler");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
