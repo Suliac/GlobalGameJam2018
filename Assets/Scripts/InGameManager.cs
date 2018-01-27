d249b3bddbf5e0380831be9e53655ba29a133c36
Suliac Blineau <suliac.blineau@orange.fr>
2018-01-27 15:06:54 +0100

    Player movement + trigger places

---
+++ Assets/Scripts/InGameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameManager : MonoBehaviour {
+
+    #region Attributes
+
+    private static InGameManager instance;
+    public static InGameManager GetSingleton
+    {
+        get { return instance; }
+    }
+
+    public int numberPlacesFound = 0;
+    #endregion
+
+    #region Initialization
+    public void SetSingleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+
+    }
+
+    private void Awake()
+    {
+        SetSingleton();
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        numberPlacesFound = 0;
+    }
+    #endregion
+
+    #region Core
+    public void PlaceFound()
+    {
+        numberPlacesFound++;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/TriggerPlace.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlace : MonoBehaviour {
+    public int NumberPlacesFoundBeforeActivation = 0;
+
+    private bool used = false;
+    public bool triggerOnEnter = true;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (triggerOnEnter)
+        {
+            SendEventPlace(other);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(!triggerOnEnter)
+        {
+            SendEventPlace(other);
+        }
+    }
+
+    public void SendEventPlace(Collider other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller && !used && InGameManager.GetSingleton.numberPlacesFound >= NumberPlacesFoundBeforeActivation)
+        {
+            print("Trigger place enter");
+            controller.CmdEnterPlace();
+            used = true;
+        }
+    }
+}
