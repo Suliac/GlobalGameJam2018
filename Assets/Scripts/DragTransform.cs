655c48915674366e4a42ae4fa50dc123372db582
Suliac Blineau <suliac.blineau@orange.fr>
2018-01-27 20:59:57 +0100

    Add profiler behavior

---
+++ Assets/Scripts/DragTransform.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class DragTransform : MonoBehaviour
+{
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+    Renderer rend;
+
+    private void Awake()
+    {
+        rend = transform.GetComponent<Renderer>();
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -9,14 +10,23 @@ public class PlayerController : NetworkBehaviour
     public GameObject copSpawn;
     public GameObject profilerSpawn;
 
+    public List<GameObject> currentNews;
+
     public bool isCop = false;
-    private GameObject spriteObject;
     private float camY;
+    private GameObject spriteObject;
+
+    private bool isDragging = false;
+    private int indexNewsDragging = -1;
+    private Vector3 lastMousePosition;
+
     // Use this for initialization
     void Start()
     {
         camY = Camera.main.transform.position.y;
         spriteObject = transform.GetChild(0).gameObject;
+        currentNews = new List<GameObject>();
+
         if (isServer)// Policier
             CopInit();
         else // Profiler
@@ -26,17 +36,17 @@ public class PlayerController : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isLocalPlayer)
-            return;
-
-        if (isCop)
+        if ((!isServer && !isLocalPlayer) || (isServer && isLocalPlayer))
         {
-            CopInput();
-            Camera.main.transform.position = new Vector3(transform.position.x, camY, transform.position.z);
-        }
-        else
-        {
-            ProfilerInput();
+            if (isCop)
+            {
+                CopInput();
+                Camera.main.transform.position = new Vector3(transform.position.x, camY, transform.position.z);
+            }
+            else
+            {
+                ProfilerInput();
+            }
         }
     }
 
@@ -62,9 +72,7 @@ public class PlayerController : NetworkBehaviour
 
         if (Physics.Raycast(ray, out hit, 100))
         {
-
             transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
-
         }
     }
 
@@ -81,16 +89,69 @@ public class PlayerController : NetworkBehaviour
         if (!isServer)
             return;
         // normalement ici = policier
-        InGameManager.GetSingleton.PlaceFound();
+        int placeNumber = InGameManager.GetSingleton.PlaceFound();
 
         // TODO : ce qu'il se passe pour le policier a ce moment
-        RpcEnterPlace();
+        RpcEnterPlace(placeNumber);
     }
     #endregion
 
     #region Profiler behaviour
     public void ProfilerInput()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (indexNewsDragging == -1) // viens de cliquer
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    if (hit.collider.gameObject.CompareTag("News"))
+                    {
+                        string name = hit.collider.gameObject.name;
+
+                        // Si on clique sur news
+                        var newsInList = currentNews.FirstOrDefault(obj => obj.GetInstanceID() == hit.collider.gameObject.GetInstanceID());
+                        if (newsInList)
+                        {
+                            currentNews.Remove(newsInList);
+                            currentNews.Add(newsInList); // objectif : le mettre a la fin
+
+                            indexNewsDragging = currentNews.LastIndexOf(newsInList);
+                            lastMousePosition = new Vector3(hit.point.x, 0, hit.point.z);
+                            //print("clique sur " + name + ", id " + indexNewsDragging + " nb list:" + currentNews.Count.ToString());
+                            ReorderNewsByPlaceInList();
+                        }
+
+                    }
+                    //transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            indexNewsDragging = -1;
+        }
+
+        if (indexNewsDragging > -1) // on est en train de drag
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                var newPosMouse = new Vector3(hit.point.x, 0, hit.point.z);
+                var diffPos = newPosMouse - lastMousePosition;
+
+                //print("Dragging from " + lastMousePosition + " to " + newPosMouse);
+
+                currentNews[indexNewsDragging].transform.position += diffPos;
+                lastMousePosition = newPosMouse;
+            }
+        }
 
     }
     public void ProfilerInit()
@@ -100,15 +161,41 @@ public class PlayerController : NetworkBehaviour
         transform.position = new Vector3(profilerSpawn.transform.position.x, -1000, profilerSpawn.transform.position.y);
     }
 
+    public void AddNews()
+    {
+
+    }
+
+    public void ReorderNewsByPlaceInList()
+    {
+        // ex : si 3 news -> le dernier est au top -> y=3, puis y=2 ...
+        int currentY = currentNews.Count;
+        if(currentY > 0)
+        {
+            for (int i = currentY; i > 0; i--)
+            {
+                int index = i - 1;
+                currentNews[index].transform.position = new Vector3(currentNews[index].transform.position.x, i, currentNews[index].transform.position.z);
+            }
+        }
+    }
+
     [ClientRpc]
-    public void RpcEnterPlace()
+    public void RpcEnterPlace(int numberPlace)
     {
         if (isServer)
             return; // already done by command
 
         print("Recu event place");
-
-        // TODO : Envoyer des journaux
+        List<GameObject> newsToPop = new List<GameObject>(InGameManager.GetSingleton.GetNewsForPlace(numberPlace).newsPrefabs);
+        List<GameObject> newsSpots = new List<GameObject>(InGameManager.GetSingleton.newsPopPoint);
+        for (int i = 0; i < newsSpots.Count; i++)
+        {
+            GameObject newsJustPoped = Instantiate(newsToPop[i], newsSpots[i].transform.position, Quaternion.Euler(90, 0, 0), InGameManager.GetSingleton.profilerView.transform);
+            currentNews.Add(newsJustPoped);
+        }
+        
+        ReorderNewsByPlaceInList();
     }
 
     #endregion
